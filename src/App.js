378cd8b816edf2d91a1a1d6f95794f6dbc84953a
Catherine Brower <catherinemb42@gmail.com>
2019-12-16 08:51:12 -0500

    remove build folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
